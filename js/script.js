f22c162fdf194756ec3ddc2c3e74bd5f4a2f3cc6
Dave Berzack <dberzack@leaderagency.com>
2014-02-02 20:11:04 -0500

    youtube test

--- js/script.js
+++ js/script.js
@@ -1,23 +1,78 @@
- $(function() {
 
-  var ytPlayer;
-  function pauseVideo() {
-   if (typeof ytPlayer != "undefined") ytPlayer.stopVideo();
-  }
+var ga = document.createElement('script');
+ga.type = 'text/javascript';
+ga.async = false;
+ga.src = 'http://www.youtube.com/player_api';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
 
-  function playVideo(){
-    ytPlayer = new YT.Player('yt1', {
+var player;
+      
+function onYouTubePlayerAPIReady() {
+//try {
+    player = new YT.Player('mainVideo', {
+      height: '390',
+      width: '640',
+      videoId: 'phzKG-Y-SI8',
       events: {
-        'onReady': function(){ currentYoutubePlayer.playVideo(); },
-        'onStateChange': function(e){ 
-          if (e.data === 0) { 
-            _gaq.push(["_trackEvent","PF2014 Video", "Finish", yTitle,0]);
-          }
-        }
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
       }
     });
+//} catch (e) { console.log(e); }
+}
+function onPlayerReady(evt) {
+    console.log('onPlayerReady', evt);
+    evt.target.playVideo();
+}
 
-  }
+var stateLabels = {"-1":"unstarted","0":"ended","1":"playing","2":"paused","3":"buffering","5":"cued"};
+var stateTimer = 0;
+function onPlayerStateChange(evt) {
+	var d = new Date();
+	newMilli = d.getMilliseconds();
+	if (stateTimer==0) stateTimer = newMilli;
+	var duration = stateTimer = newMilli;
+	stateTimer = newMilli;
+    console.log('... ', evt.data+":"+stateLabels[evt.data]+" > "+duration+"ms" );
 
+    if (evt.data == YT.PlayerState.PLAYING) {
+       //started playing
+    }
+}
+function pauseVideo() {
+    console.log('pauseVideo');
+    player.pauseVideo();
+}
+function playVideo(secs){
+	if (typeof secs != "undefined" && !isNaN(secs) ) player.seekTo(secs);
+	player.playVideo();
+}
+function cueVideo(secs){
+	if (typeof secs != "undefined" && !isNaN(secs) ) player.seekTo(secs);
+	player.pauseVideo();
+}
+
+//.getCurrentTime()
+
+$(function() {
+  $("#pause").click(function(){
+  	pauseVideo();
+  })       
+  $("#play").click(function(){
+  	playVideo();
+  })      
+  $("#play20").click(function(){
+  	playVideo(20)
+  })       
+  $("#play590").click(function(){
+  	playVideo(590)
+  })      
+  $("#cue20").click(function(){
+  	cueVideo(20)
+  })       
+  $("#cue590").click(function(){
+  	cueVideo(590)
+  })        
 
 });
